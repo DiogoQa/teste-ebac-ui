32377fd248c52a5d2a5d39d59859fbf586cfc165
diogo <diogobarbosa.sqa@gmail.com>
2022-12-28 12:49:12 -0300

    usando massa de dados em lista

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page';
-
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -16,4 +16,27 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
        EnderecoPage.editarEnderecoFaturamento('Diogo', 'Santos', 'jv.teste', 'Brasil', 'Av. paulista', '164', 'São Paulo', 'São Paulo', '08565300', '11999999', 'diogo@teste.com')
        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando massa de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
+     });
+ 
+
+
+
+
+
 });
