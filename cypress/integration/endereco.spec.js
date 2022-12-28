a505b7b184850139566c9ceeda46be5bbffb7be1
diogo <diogobarbosa.sqa@gmail.com>
2022-12-28 12:06:48 -0300

    utilizando page objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="Cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page';
+
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -7,10 +9,11 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
             cy.login(dados.usuario, dados.senha)
         })
         
-    });
+     });
     
      
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-       
+       EnderecoPage.editarEnderecoFaturamento('Diogo', 'Santos', 'jv.teste', 'Brasil', 'Av. paulista', '164', 'São Paulo', 'São Paulo', '08565300', '11999999', 'diogo@teste.com')
+       cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
